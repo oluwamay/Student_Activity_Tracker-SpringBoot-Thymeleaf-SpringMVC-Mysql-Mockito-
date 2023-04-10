784dd58b17b184ad4ec411933f9ef8d9a5ee77aa
oluwamay <samuelmayowao060@gmail.com>
2023-04-10 23:55:55 +0100

    initial commit

---
+++ src/main/java/com/studentactivitytracker/StudentActivityTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.studentactivitytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentActivityTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentActivityTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/studentactivitytracker/StudentActivityTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentactivitytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentActivityTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
